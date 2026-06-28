cdec226173aa15d4a520612c8e93d04edb5dab38
Hasindu Indusara <hasinduindusara133@gmail.com>
2026-06-28 12:04:26 +0530

    Update TravelAppApplication.java

--- src/main/java/com/example/TravelApp/TravelAppApplication.java
+++ src/main/java/com/example/TravelApp/TravelAppApplication.java
@@ -2,12 +2,14 @@ package com.example.TravelApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync; // 🎯 මේ import එක ඉබේම වැටෙයි, නැත්නම් දාන්න.
 
 @SpringBootApplication
+@EnableAsync // 🚀 මෙන්න මෙතනට තමයි මේක එකතු කරන්න ඕනේ!
 public class TravelAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAppApplication.class, args);
+    }
 
 }
